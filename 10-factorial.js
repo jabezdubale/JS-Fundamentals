9abd9e535c7ca54b473056e92aa70e99c70e0a4a
jabezdubale <jabezdubale6@gmail.com>
2025-05-26 00:22:19 +0300

    Factorial

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,8 +1,8 @@
 const val = parseInt(process.argv[2]);
 
-function facto(n) {
+function factorial(n) {
   if (Number.isNaN(n) || n === 0) return 1;
-  return n * factorial(n - 1);
+  return n * factorial(n-1);
 }
 
 console.log(factorial(val));
